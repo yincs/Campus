60dabb5fd69f82e5f86396f6ce60721a8511c676
changsong_yin <897710401@qq.com>
2016-10-09 19:01:27 +0800

    init

---
+++ Dev/Service/campus1/campus-base/campus-model/src/main/java/org/changs/campus/model/App.java
@@ -0,0 +1,13 @@
+package org.changs.campus.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
